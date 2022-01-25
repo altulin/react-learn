a74812b94fbd0e2a8bf8e8544e98dbf420da5cf0
unknown <al.tulin@gmail.com>
2022-01-25 20:14:11 +0800

    refactoring_2

--- src/components/burger-constructor/BurgerConstructor.tsx
+++ src/components/burger-constructor/BurgerConstructor.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './BurgerConstructor.module.css';
-import { CurrencyIcon, LockIcon, DragIcon, DeleteIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { CurrencyIcon, ConstructorElement, Button, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import data  from '../../utils/data';
 
 type ButtonConstructorProps = {
@@ -17,63 +17,6 @@ const ButtonConstructor = ({position}:ButtonConstructorProps) => {
 	)
 }
 
-type StatusConstructorProps = {
-	position?: boolean
-}
-
-const StatusConstructor = ({position}: StatusConstructorProps) => {
-	const lock = (<span className={styles.constructor_status}>
-		<LockIcon type="secondary"/>
-	</span>)
-	const del = (<span className={styles.constructor_status}>
-		<DeleteIcon type="primary" />
-	</span>)
-	return position?lock:del
-
-}
-
-
-
-class BoxConstructor extends React.Component<BoxConstructorProps> {
-	constructor(props :BoxConstructorProps) {
-		super(props);
-		this.state = {
-			top: false,
-			bottom: false
-		}
-	}
-
-	top_style = styles.constructor_content_top;
-	bottom_style = styles.constructor_content_bottom;
-
-	render() {
-		return (
-				<>
-					<ButtonConstructor position={this.props.position_top || this.props.position_bottom ?true:false}/>
-
-					<div className={`${styles.constructor_content} ${this.props.position_top ? this.top_style : ''} ${this.props.position_bottom ? this.bottom_style : ''}`}>
-						<figure className={styles.constructor_img_wrap}>
-							<img className={styles.constructor_img} src={this.props.image} alt="" width={80} height={40}/>
-						</figure>
-						<h3 className={`${styles.constructor_title} text text_type_main-default`}>{this.props.name} {this.props.position_top ? '(верх)':''} {this.props.position_bottom ? '(низ)':''}</h3>
-						<p className={styles.constructor_price_box}>
-							<span className={`${styles.constructor_price} text text_type_digits-default mr-2`}>30</span>
-							<CurrencyIcon type="primary" />
-						</p>
-						<StatusConstructor position={this.props.position_top || this.props.position_bottom ?true:false}/>
-					</div>
-				</>
-		)
-	}
-}
-
-interface BoxConstructorProps {
-	image: string,
-	position_top?: boolean,
-	position_bottom?: boolean,
-	name: string,
-}
-
 class BurgerConstructor extends React.Component {
 
 	getList = () => {
@@ -82,16 +25,30 @@ class BurgerConstructor extends React.Component {
 
 	render() {
 		return (
-			<section className={styles.constructor_section}>
+			<section className={`constructor_section ${styles.constructor_section}`}>
 				<div className={`${styles.constructor_header} ${styles.constructor_box}`}>
-					<BoxConstructor image={data[0].image_mobile} name={data[0].name} position_top/>
+					{/* <BoxConstructor image={data[0].image_mobile} name={data[0].name} position_top/> */}
+					<ButtonConstructor position/>
+					<ConstructorElement
+						text={`${data[0].name} (верх)`}
+						price={data[0].price}
+						thumbnail={data[0].image_mobile}
+						type="top"
+						isLocked={true}
+					/>
 				</div>
 
 				<ul className={styles.constructor_list}>
 					{this.getList().map((item)=>
 
-						<li key={item._id} className={`${styles.constructor_item} ${styles.constructor_box}`}>
-							<BoxConstructor image={item.image_mobile} name={item.name}/>
+						<li key={item._id} className={`constructor_item ${styles.constructor_box}`}>
+							<ButtonConstructor/>
+							<ConstructorElement
+								text={item.name}
+								price={item.price}
+								thumbnail={item.image_mobile}
+								isLocked={false}
+							/>
 						</li>
 					)}
 
@@ -99,7 +56,14 @@ class BurgerConstructor extends React.Component {
 				</ul>
 
 				<div className={`${styles.constructor_footer} ${styles.constructor_box}`}>
-					<BoxConstructor image={data[0].image_mobile} name={data[0].name} position_bottom/>
+					<ButtonConstructor position/>
+					<ConstructorElement
+						text={`${data[0].name} (низ)`}
+						price={data[0].price}
+						thumbnail={data[0].image_mobile}
+						type="bottom"
+						isLocked={true}
+					/>
 				</div>
 
 				<div className={styles.constructor_number}>
@@ -107,9 +71,11 @@ class BurgerConstructor extends React.Component {
 						<span className={`${styles.constructor_price} text text_type_digits-medium`}>6100</span>
 						<CurrencyIcon type="primary" />
 					</p>
-					<button className={`${styles.constructor_number_btn} text text_type_main-default`}>Оформить заказ</button>
-				</div>
 
+					<Button type="primary" size="large">
+						Оформить заказ
+					</Button>
+				</div>
 			</section>
 		);
 	}
